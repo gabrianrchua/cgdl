1c1cc23bf3027ffd121ee0ce0cc31961ab8f40bb
Gabrian Chua <gabrianrchua@gmail.com>
2023-12-18 22:51:37 -0500

    feat: basic db testing

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const sqlite3 = require('sqlite3');
+const prompt = require('prompt-sync')({sigint: true});
+
 let db = new sqlite3.Database('./test.db', sqlite3.OPEN_READWRITE, (err) => {
-	if (err.errno == 14) {
+	if (err && err.errno == 14) {
 		db = new sqlite3.Database('./test.db', sqlite3.CREATE, (err) => {
 			console.error("Fatal: Error creating database at ./test.db");
 			console.error(err);
 			process.exit(1);
-		}
-	} else {
+		});
+		createDb(db);
+	} else if (err) {
 		console.error("Fatal: Error loading database at ./test.db");
 		console.error(err);
 		process.exit(1);
+	} else {
+		testQuery(db);
 	}
 });
 
@@ -36,12 +41,41 @@ function createDb(database) {
 	CREATE TABLE Android LIKE PC;
 	CREATE TABLE Switch LIKE PC;
 	CREATE TABLE Wii LIKE PC;
-	CREATE TABLE Files LIKE PC;`, () => {
-		console.log("First time database created and initialized successfully");
-	}
+	CREATE TABLE Files LIKE PC;`, (err) => {
+		if (err != null) {
+			console.log("First time database created and initialized successfully");
+		} else {
+			console.error(err);
+		}
+		testQuery(database);
+	});
 }
 
+function testQuery(database) {
+	database.each("SELECT rowid AS id, * FROM Categories", (err, row) => {
+		console.log(row.id + " | " + row.Name + " | " + row.Description);
+	}, (err) => {
+		console.log("done");
+		terminate();
+	});
+}
+
+/*let userin = ""
+while (userin != "exit") {
+	userin = prompt("db> ").toLowerCase();
+	if (userin == "exit") {
+		terminate();
+	}
+}*/
+
 function terminate(exitcode = 0) {
-	db.close();
-	process.exit(exitcode)
+	db.close(err => {
+		if (err != null) {
+			console.error("Error while closing db:");
+			console.error(err);
+			process.exit(1);
+		} else {
+			process.exit(exitcode);
+		}
+	});
 }
