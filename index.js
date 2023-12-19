6e987890e62eff477307d392d584be5947f4ef94
Gabrian Chua <gabrianrchua@gmail.com>
2023-12-19 10:11:23 -0500

    fix: bug creating db

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
-const sqlite3 = require('sqlite3');
-const prompt = require('prompt-sync')({sigint: true});
+const sqlite3 = require('sqlite3').verbose();
+//const prompt = require('prompt-sync')({sigint: true});
+const express = require('express');
+const app = express();
+const api = express();
+const PORT = 8080;
+const GOOD_REQ_RE = /^[a-z0-9]+$/gi;
+
+app.use(express.json());
+app.use("/api", api);
 
 let db = new sqlite3.Database('./test.db', sqlite3.OPEN_READWRITE, (err) => {
-	if (err && err.errno == 14) {
-		db = new sqlite3.Database('./test.db', sqlite3.CREATE, (err) => {
-			console.error("Fatal: Error creating database at ./test.db");
+	if (err) {
+		if (err.errno == 14) {
+			db = new sqlite3.Database('./test.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+				if (err) {
+					console.error("Fatal: Error creating database at ./test.db");
+					console.error(err);
+					process.exit(1);
+				} else {
+					initDb(db);
+				}
+			});
+		} else {
+			console.error("Fatal: Error loading database at ./test.db");
 			console.error(err);
 			process.exit(1);
-		});
-		createDb(db);
-	} else if (err) {
-		console.error("Fatal: Error loading database at ./test.db");
-		console.error(err);
-		process.exit(1);
+		}
 	} else {
-		testQuery(db);
+		initApp();
 	}
 });
 
+function initApp() {
+	app.listen(PORT, () => {
+		console.log("Listening on port " + PORT);
+	});
+}
+
 // create db structure and empty fields
-function createDb(database) {
+function initDb(database) {
 	database.exec(`
 	CREATE TABLE Categories
 	(
@@ -38,19 +57,20 @@ function createDb(database) {
 	Description varchar(255),
 	Link varchar(64)
 	);
-	CREATE TABLE Android LIKE PC;
-	CREATE TABLE Switch LIKE PC;
-	CREATE TABLE Wii LIKE PC;
-	CREATE TABLE Files LIKE PC;`, (err) => {
-		if (err != null) {
-			console.log("First time database created and initialized successfully");
-		} else {
+	CREATE TABLE Android AS SELECT * FROM PC WHERE 0;
+	CREATE TABLE Switch AS SELECT * FROM PC WHERE 0;
+	CREATE TABLE Wii AS SELECT * FROM PC WHERE 0;
+	CREATE TABLE Files AS SELECT * FROM PC WHERE 0;`, (err) => {
+		if (err) {
 			console.error(err);
+		} else {
+			console.log("First time database created and initialized successfully");
 		}
-		testQuery(database);
+		initApp();
 	});
 }
 
+// runs test query on database and then terminates server
 function testQuery(database) {
 	database.each("SELECT rowid AS id, * FROM Categories", (err, row) => {
 		console.log(row.id + " | " + row.Name + " | " + row.Description);
@@ -79,3 +99,52 @@ function terminate(exitcode = 0) {
 		}
 	});
 }
+
+app.get("/", (req, res) => {
+	res.send("APP home");
+});
+
+api.get("/", (req, res) => {
+	res.send({
+		"status": "healthy"
+	});
+});
+api.get("/categories", (req, res) => {
+	db.all("SELECT rowid AS id, * FROM Categories", (err, rows) => {
+		if (err != null) {
+			res.send({
+				"status": "error",
+				"err": err
+			});
+		} else {
+			res.send({
+				"status": "success",
+				"result": rows
+			});
+		}
+	});
+});
+api.get("/categories/:name", (req, res) => {
+	let name = req.params["name"];
+	let match = name.match(GOOD_REQ_RE);
+	if (match && match.length == 1) {
+		db.all("SELECT rowid AS id, * FROM " + name, (err, rows) => {
+			if (err != null) {
+				res.send({
+					"status": "error",
+					"err": err
+				});
+			} else {
+				res.send({
+					"status": "success",
+					"result": rows
+				});
+			}
+		});
+	} else {
+		res.send({
+			"status": "error",
+			"err": "Invalid or malformed request"
+		});
+	}
+});
