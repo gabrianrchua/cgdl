0875f60e39fc73d9ade68fca04142d69e038f862
Gabrian Chua <gabrianrchua@gmail.com>
2023-12-20 11:29:38 -0500

    feat: add page title and favicon

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { CategoryViewComponent } from './category-view/category-view.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: ':id', component: CategoryViewComponent },
+    { path: '', component: HomeComponent, title: "CG Download Center" },
+    { path: ':id', component: CategoryViewComponent, title: "CG Download Center" },
     { path: '**', redirectTo: '' }
 ];
