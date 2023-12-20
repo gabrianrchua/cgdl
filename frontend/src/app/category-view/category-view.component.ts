2bcc614069a2a35994e63253d87004862b10366e
Gabrian Chua <gabrianrchua@gmail.com>
2023-12-19 21:22:30 -0500

    feat: create placeholder category view

--- frontend/src/app/category-view/category-view.component.ts
+++ frontend/src/app/category-view/category-view.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-category-view',
   standalone: true,
-  imports: [],
+  imports: [ MatButtonModule, MatIconModule, RouterModule ],
   templateUrl: './category-view.component.html',
   styleUrl: './category-view.component.scss'
 })
