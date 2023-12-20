d17241dc9e567194e8778e6be185693afb63da6d
Gabrian Chua <gabrianrchua@gmail.com>
2023-12-19 21:14:29 -0500

    feat: add placeholder to home

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ MatButtonModule, MatIconModule, RouterModule ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
